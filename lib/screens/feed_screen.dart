f484cc30130495ef50a385b4ff209c3821fbcf41
CruthOver <b3ng.b3ng16@gmail.com>
2023-08-28 02:23:15 +0700

    Halaman Baru untuk Paket Haji dan Umrah, dan penambahan widget wallet di feed screen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -22,7 +22,7 @@ class FeedScreen extends StatefulWidget {
 
 class _FeedScreenState extends State<FeedScreen> {
   final CarouselController _controller = CarouselController();
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -59,202 +59,220 @@ class _FeedScreenState extends State<FeedScreen> {
                 ),
               ],
             ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            child: Card(
-              color: Colors.lightBlue,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: CarouselSlider(
-                      items: [
-                        Card(
-                          margin: const EdgeInsets.symmetric(horizontal: 16),
-                          color: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: const [
-                                Text(
-                                  "DCC Wallet",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18.0),
-                                ),
-                                Text(
-                                  "0 Coins",
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 16.0),
-                                ),
-                                Text(
-                                  "IDR 0",
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 16.0),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Card(
-                          margin: const EdgeInsets.symmetric(horizontal: 16),
-                          color: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Text(
-                                  "Wallet Address",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18.0),
-                                ),
-                                Row(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: 130,
+              collapsedHeight: 125,
+              floating: true,
+              snap: true,
+              automaticallyImplyLeading: false,
+              backgroundColor: mobileBackgroundColor,
+              flexibleSpace: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: Card(
+                  color: Colors.lightBlue,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: CarouselSlider(
+                          items: [
+                            Card(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 8),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Flexible(
-                                      child: Text(
-                                        sha256
-                                            .convert(utf8.encode(
-                                                _auth.currentUser!.email!))
-                                            .toString(),
-                                        style: const TextStyle(
+                                  children: const [
+                                    Text(
+                                      "DCC Wallet",
+                                      style: TextStyle(
                                           color: Colors.black,
-                                          fontSize: 16.0,
-                                        ),
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 2,
-                                      ),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18.0),
                                     ),
-                                    const SizedBox(
-                                      width: 16,
+                                    Text(
+                                      "0 Coins",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 16.0),
                                     ),
-                                    TextButton(
-                                      onPressed: () async {
-                                        await Clipboard.setData(ClipboardData(
-                                                text: sha256
-                                                    .convert(utf8.encode(_auth
-                                                        .currentUser!.email!))
-                                                    .toString()))
-                                            .then((value) {
-                                          showSnackBar(context, "Copied");
-                                        });
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                          padding: EdgeInsets.zero,
-                                          minimumSize: const Size(50, 30),
-                                          tapTargetSize:
-                                              MaterialTapTargetSize.shrinkWrap,
-                                          alignment: Alignment.center),
-                                      child: const Text("Copy"),
+                                    Text(
+                                      "IDR 0",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 16.0),
                                     ),
                                   ],
                                 ),
-                              ],
+                              ),
+                            ),
+                            Card(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 8),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Text(
+                                      "Wallet Address",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18.0),
+                                    ),
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Flexible(
+                                          child: Text(
+                                            sha256
+                                                .convert(utf8.encode(
+                                                    _auth.currentUser!.email!))
+                                                .toString(),
+                                            style: const TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 16.0,
+                                            ),
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 2,
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          width: 16,
+                                        ),
+                                        TextButton(
+                                          onPressed: () async {
+                                            await Clipboard.setData(
+                                                    ClipboardData(
+                                                        text: sha256
+                                                            .convert(utf8
+                                                                .encode(_auth
+                                                                    .currentUser!
+                                                                    .email!))
+                                                            .toString()))
+                                                .then((value) {
+                                              showSnackBar(context, "Copied");
+                                            });
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                              padding: EdgeInsets.zero,
+                                              minimumSize: const Size(50, 30),
+                                              tapTargetSize:
+                                                  MaterialTapTargetSize
+                                                      .shrinkWrap,
+                                              alignment: Alignment.center),
+                                          child: const Text("Copy"),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                          options: CarouselOptions(
+                            scrollDirection: Axis.vertical,
+                            autoPlay: false,
+                            viewportFraction: 0.7,
+                            aspectRatio: 2.0,
+                            enableInfiniteScroll: false,
+                          ),
+                          carouselController: _controller,
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Icon(
+                              Icons.arrow_upward,
+                              color: Colors.black,
+                              size: 16,
                             ),
                           ),
-                        ),
-                      ],
-                      options: CarouselOptions(
-                        scrollDirection: Axis.vertical,
-                        autoPlay: false,
-                        viewportFraction: 0.7,
-                        aspectRatio: 2.0,
-                        enableInfiniteScroll: false,
+                          const Text(
+                            "Pay",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
                       ),
-                      carouselController: _controller,
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Icon(
-                          Icons.arrow_upward,
-                          color: Colors.black,
-                          size: 16,
-                        ),
-                      ),
-                      const Text(
-                        "Pay",
-                        style: TextStyle(color: Colors.white),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Icon(
+                              Icons.add,
+                              color: Colors.black,
+                              size: 16,
+                            ),
+                          ),
+                          const Text(
+                            "Top Up",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Icon(
-                          Icons.add,
-                          color: Colors.black,
-                          size: 16,
-                        ),
-                      ),
-                      const Text(
-                        "Top Up",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            child: StreamBuilder(
-              stream:
-                  FirebaseFirestore.instance.collection('posts').snapshots(),
-              builder: (context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (ctx, index) => Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: width > webScreenSize ? width * 0.3 : 0,
-                      vertical: width > webScreenSize ? 15 : 0,
-                    ),
-                    child: PostCard(
-                      snap: snapshot.data!.docs[index].data(),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+            )
+          ];
+        },
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (ctx, index) => Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: width > webScreenSize ? width * 0.3 : 0,
+                  vertical: width > webScreenSize ? 15 : 0,
+                ),
+                child: PostCard(
+                  snap: snapshot.data!.docs[index].data(),
+                ),
+              ),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
